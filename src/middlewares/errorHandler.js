4359f85edef51f94d98e1439d1bc967efaf4764c
Volodymyr892 <sevcukvolodimir896@gmail.com>
2024-09-11 22:21:51 +0300

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,19 @@
+import createHttpError from "http-errors";
+
 export const errorHandler = (err,reg,res, next)=>{
+
+    if (err instanceof createHttpError.HttpError) {
+        res.status(err.status || 500).json({
+            status: err.status || 500, 
+            message: err.message,      
+            data: null                
+        });
+        return;
+    }
     res.status(500).json({
+        status: 500,
         message: 'Something went wrong',
         error: err.message,
+        data: null     
     });
 };

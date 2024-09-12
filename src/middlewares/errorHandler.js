e6f6e3f10cc491e9f132e2bc096300651d7819ae
Volodymyr892 <sevcukvolodimir896@gmail.com>
2024-09-12 10:46:58 +0300

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -6,14 +6,13 @@ export const errorHandler = (err,reg,res, next)=>{
         res.status(err.status || 500).json({
             status: err.status || 500, 
             message: err.message,      
-            data: null                
+            data: err               
         });
         return;
     }
     res.status(500).json({
         status: 500,
         message: 'Something went wrong',
-        error: err.message,
-        data: null     
+        error: err.message,   
     });
 };
